0fdde5bab31f2aa9139b79932c0bf9868226e9e2
sreypov <sreypov.poch@student.cadt.edu.kh>
2024-10-28 20:11:08 +0700

    updated

--- S2 - PRACTICE - Manipulate basic widgets/lib/S2 pratice/w3-s2-ex4-widget.dart
+++ S2 - PRACTICE - Manipulate basic widgets/lib/S2 pratice/w3-s2-ex4-widget.dart
@@ -6,9 +6,19 @@ void main() {
       padding: const EdgeInsets.all(160),
       child: const Column(
         children: [
-          CustomCard(text: 'OOP', color: Color.fromARGB(255, 77, 199, 255)),
-          CustomCard(text: 'DART', color: Color.fromARGB(255, 76, 174, 255)),
-          CustomCard(text: 'FLUTTER', color: Color.fromARGB(255, 15, 138, 239)),
+          CustomCard(
+            text: 'OOP',
+            start: Colors.lightBlue,
+            end: Colors.blue,
+          ),
+          CustomCard(
+              text: 'DART',
+              start: Color.fromARGB(255, 230, 108, 99),
+              end: Color.fromARGB(255, 240, 33, 18)),
+          CustomCard(
+              text: 'FLUTTER',
+              start: Color.fromARGB(255, 237, 225, 114),
+              end: Color.fromARGB(255, 253, 229, 14)),
         ],
       ),
     ),
@@ -17,15 +27,19 @@ void main() {
 
 class CustomCard extends StatelessWidget {
   final String text;
-  final Color color;
-  const CustomCard({super.key, required this.text, required this.color});
+  final Color start;
+  final Color end;
+  const CustomCard(
+      {super.key, required this.text, required this.start, required this.end});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: color,
+        gradient: LinearGradient(
+          colors: [start, end],
+        ),
         borderRadius: const BorderRadius.all(Radius.circular(50)),
       ),
       child: Center(
